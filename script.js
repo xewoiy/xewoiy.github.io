fe9592ab6ff37e95cba83d97eb0dceb5dfe649bc
xewoiy <matveyjacyn5@yandex.ru>
2024-01-01 15:39:52 +0700

    so big updt

---
+++ script.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+    $("#socials_open").on("click", function(){
+    $("#overlay_socials").fadeIn(300);
+    $("#popup_socials").fadeIn(300);
+    });
+
+    $("#socials_close").on("click", function(){
+    $("#overlay_socials").fadeOut(300);
+    $("#popup_socials").fadeOut(300);
+    });
+});
+
+$(document).ready(function(){
+    $("#games_open").on("click", function(){
+    $("#overlay_games").fadeIn(300);
+    $("#popup_games").fadeIn(300);
+    });
+
+    $("#games_close").on("click", function(){
+    $("#overlay_games").fadeOut(300);
+    $("#popup_games").fadeOut(300);
+    });
+});
+
+$(document).ready(function(){
+    $("#descg").on("click", function(){
+    $("#overlay_descg").fadeIn(300);
+    $("#popup_descg").fadeIn(300);
+    });
+
+    $("#descg_close").on("click", function(){
+    $("#overlay_descg").fadeOut(300);
+    $("#popup_descg").fadeOut(300);
+    });
+});
+
+$(document).ready(function(){
+    $("#channels").on("click", function(){
+    $("#overlay_channels").fadeIn(300);
+    $("#popup_channels").fadeIn(300);
+    });
+
+    $("#channels_close").on("click", function(){
+    $("#overlay_channels").fadeOut(300);
+    $("#popup_channels").fadeOut(300);
+    });
+});
+
+$(document).ready(function(){
+    $("#other_open").on("click", function(){
+    $("#overlay_other").fadeIn(300);
+    $("#popup_other").fadeIn(300);
+    });
+
+    $("#other_close").on("click", function(){
+    $("#overlay_other").fadeOut(300);
+    $("#popup_other").fadeOut(300);
+    });
+});
+
+$(document).ready(function(){
+    $("#hideimg").on("click", function(){
+    $("#imgv").fadeOut(300);
+    });
+});
